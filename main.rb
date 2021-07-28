ee5e3871e3e079a23bd41cf48ccf21f9a98d2c5c
Pandademic <stewie.gupte@gmail.com>
2021-07-28 13:05:56 -0400

    Url checking so no blank urls appear

--- main.rb
+++ main.rb
@@ -1,9 +1,20 @@
 require "down"
 def installWithUrl(url)
     File.open("info.log", "w") { |f| f.write "#{Time.now} - img -Image download started\n" }
-    puts "Downloading file from #{url}" 
-    Down.download("#{url}", destination: "cups/img/")
-    puts "Download complete"
-    File.open("info.log", "w") { |f| f.write "#{Time.now} - img -Image download Complete\n" }
+    if "#{url}"==""
+        puts "fatal Error:No url"
+        puts "Download failed"
+        File.open("info.log", "w") { |f| f.write "#{Time.now} - FATAL:No url\n" }
+    elsif "#{url}"==" " 
+        puts "fatal Error:No url"
+        puts "Download failed"
+        File.open("info.log", "w") { |f| f.write "#{Time.now} - FATAL:No url\n" }
+    else
+        puts "Downloading file from #{url}" 
+        Down.download("#{url}", destination: "cups/WithUrl/")
+        puts "Download complete"
+        File.open("info.log", "w") { |f| f.write "#{Time.now} - img -Image download Complete\n" }
+
+    end
 end
-installWithUrl "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12225358/Pug-On-White-01.jpg"
+installWithUrl ""
