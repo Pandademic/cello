484556d0365bbd2cff725b9f6941ec1d453046bb
Pandademic <stewie.gupte@gmail.com>
2021-07-28 13:00:39 -0400

    Naming changes

--- main.rb
+++ main.rb
@@ -1,9 +1,9 @@
 require "down"
-def getImage(url)
+def installWithUrl(url)
     File.open("info.log", "w") { |f| f.write "#{Time.now} - img -Image download started\n" }
     puts "Downloading file from #{url}" 
     Down.download("#{url}", destination: "cups/img/")
     puts "Download complete"
     File.open("info.log", "w") { |f| f.write "#{Time.now} - img -Image download Complete\n" }
 end
-getImage("https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12225358/Pug-On-White-01.jpg")
+installWithUrl "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12225358/Pug-On-White-01.jpg"
