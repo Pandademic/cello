581184d75c73246ee1db25c2e700e37b9db0bd51
Pandademic <stewie.gupte@gmail.com>
2021-08-03 11:42:20 -0400

    ARGV

--- latte.rb
+++ latte.rb
@@ -4,4 +4,13 @@ if ARGV.first =="help"
      puts "Latte help sytem"
      puts "To install a image from a url try latte InstallImage 'url of image' "
      puts "for more help try the docs"
+elsif ARGV.size <1
+     abort("ERROR:No task specified")
+end
+if ARGV.first== "InstallImg"
+     @url=ARGV[1]
+     Down.download("#@url", destination: "cups/WithUrl/")
+
+else
+    abort("This task does not exist")
 end
