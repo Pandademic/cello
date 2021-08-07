8ded673f329f7c9e23e1dcd0e1d9a7f7dd389fce
Pandademic <stewie.gupte@gmail.com>
2021-08-07 17:16:57 -0400

    modules && pkg alpha

--- core/latte.rb
+++ core/latte.rb
@@ -2,28 +2,47 @@
 
 Bundler.require(:default)
 abort('ERROR:No task specified') while ARGV.empty?
-module Helper
-   def helper.help
-    puts 'Latte help sytem'
-    puts "To install a image from a url try latte InstallImage 'url of image' "
-   end
+module Core
+  module Helper
+    def Core.helper.help
+      puts 'Latte help sytem'
+      puts "To install a image from a url try latte InstallImage 'url of image' "
+    end
+  end
+  module Image
+    def Core.Image.main(url)
+      @url = url
+      if @url.to_s == ''
+        abort('This URL is non-existent')
+      else
+        File.open('info.log', 'w') { |f| f.write "#{Time.now} - img -Image download started\n" }
+        Down.download(@url.to_s, destination: 'cups/WithUrl/')
+      end
+    end
+  end
+  module pkg
+    def Core.pkg.find(query)
+      $query=query
+      $destination="https://github.com/Pandademic/Latte/packages/#$query.ini"
+      $File=IniFile.load("#$destination")
+      $data=File["package"]
+      $source=data["Source"]
+      Core.Debug.find()
+      #download($source)
+    end
+  end
+  module Debug
+    def find()
+      puts "query: #$query"
+      puts "destination:#$destination"
+      puts "sourceUrl:#$source"
+    end
+  end
 end
-module Image
-   def Image.Install()
-    @url = ARGV[1]
-    if @url.to_s == ''
-      abort('This URL is non-existent')
-     else
-      File.open('info.log', 'w') { |f| f.write "#{Time.now} - img -Image download started\n" }
-      Down.download(@url.to_s, destination: 'cups/WithUrl/')
-   end
-    
-   end
-end
+
 FileUtils.touch('info.log')
-case Mycase
- when ARGV.first == 'help'
-   Helper.help
- when ARGV.first == 'InstallImg'
-   Image.Install
-  
+when ARGV.first == 'help'
+  Core.Helper.help
+when ARGV.first == 'InstallImg'
+  Core.Image.main(ARGV[1])
+
