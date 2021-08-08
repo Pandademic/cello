67efac2028d22e89aa04a9657e9b653153bb48e5
codefactor-io <support@codefactor.io>
2021-08-08 14:40:57 +0000

    [CodeFactor] Apply fixes to commit c86f934
    [ci skip] [skip ci]

--- core/latte.rb
+++ core/latte.rb
@@ -1,45 +1,48 @@
 # frozen_string_literal: true
-require "Down"
-require "inifile"
-require "fileutils"
-require "bundler"
-#Bundler.require(:default)
+
+require 'Down'
+require 'inifile'
+require 'fileutils'
+require 'bundler'
+# Bundler.require(:default)
 abort('ERROR:No task specified') while ARGV.empty?
 module Helper
-		def Helper.help
-      puts 'Latte help sytem'
-      puts "To install a image from a url try latte InstallImage 'url of image' "
-    end
+  def self.help
+    puts 'Latte help sytem'
+    puts "To install a image from a url try latte InstallImage 'url of image' "
+  end
 end
+
 module Image
-	def Image.main(url)
-      @url = url
-      if @url.to_s == ''
-        abort('This URL is non-existent')
-      else
-        File.open('info.log', 'w') { |f| f.write "#{Time.now} - img -Image download started\n" }
-        Down.download(@url.to_s, destination: 'cups/WithUrl/')
-      end
+  def self.main(url)
+    @url = url
+    if @url.to_s == ''
+      abort('This URL is non-existent')
+    else
+      File.open('info.log', 'w') { |f| f.write "#{Time.now} - img -Image download started\n" }
+      Down.download(@url.to_s, destination: 'cups/WithUrl/')
+    end
     end
 end
+
 module Pkg
-    def Pkg.find(query)
-     	$query=query
-     	$destination="https://github.com/Pandademic/Latte/packages/#$query.ini"
-      $File=IniFile.load("#$destination")
-      $data=File["package"]
-      $source=data["Source"]
-      puts "query: #$query"
-      puts "destination:#$destination"
-      puts "sourceUrl:#$source"
-		end
+  def self.find(query)
+    $query = query
+    $destination = "https://github.com/Pandademic/Latte/packages/#{$query}.ini"
+    $File = IniFile.load($destination.to_s)
+    $data = File['package']
+    $source = data['Source']
+    puts "query: #{$query}"
+    puts "destination:#{$destination}"
+    puts "sourceUrl:#{$source}"
+    end
 end
 
 FileUtils.touch('info.log')
-#when ARGV.first == 'help'
-while ARGV[0]=="help"
+# when ARGV.first == 'help'
+while ARGV[0] == 'help'
   Helper.help
-#FIXME: #4 Infinte loop  
+  # FIXME: #4 Infinte loop
 end
-#when ARGV.first == 'InstallImg'
-#Core.Image.main(ARGV[1])ru
+# when ARGV.first == 'InstallImg'
+# Core.Image.main(ARGV[1])ru
