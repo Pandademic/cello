aae18cb6ea795790e9fd4d9ecc7ff23ec0fe11b2
Pandademic <74566464+Pandademic@users.noreply.github.com>
2021-09-03 08:48:30 -0400

    Update latte.rb

--- core/latte.rb
+++ core/latte.rb
@@ -36,7 +36,7 @@ module Pkg
     else
       @ISC = pkgdata['Isc'] # install command
       puts 'running Install command as specified in Pkgfile'
-      puts 'this is the install command #@ISC"
+      puts 'this is the install command #@ISC '
       system(@ISC.to_s)
     end
   end
