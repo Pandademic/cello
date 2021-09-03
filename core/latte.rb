4365c39a81606f5b9bd70f8610cacf2e4eafc269
codefactor-io <support@codefactor.io>
2021-09-03 12:52:16 +0000

    [CodeFactor] Apply fixes to commit daca7de
    [ci skip] [skip ci]

--- core/latte.rb
+++ core/latte.rb
@@ -36,7 +36,7 @@ module Pkg
     else
       $isc = pkgdata['Isc'] # install command
       puts 'running Install command as specified in Pkgfile'
-      puts  $isc
+      puts $isc
       system(@ISC.to_s)
     end
   end
