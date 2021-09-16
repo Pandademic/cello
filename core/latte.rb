487fcc6106825f5fdfddde58f52bfafd218c26d5
Pandademic <stewie.gupte@gmail.com>
2021-09-16 16:22:10 +0000

    change error messages

--- core/latte.rb
+++ core/latte.rb
@@ -16,12 +16,9 @@ module Pkg
     pfr = URI.open("https://raw.githubusercontent.com/Pandademic/Latte/master/pkgs/#{$query}.ini").read
     packageFileURL = "https://raw.githubusercontent.com/Pandademic/Latte/master/pkgs/#{$query}.ini"
     puts "Package file:#{pfr}"
-
     system("curl  -O #{packageFileURL}")
     puts 'package file download complete'
     Pkg.downloadLatest
-    # TODO: implement begin.resuce,else,ensure,end
-    # TODO: changes global vars to :: scopes
   end
 
   def self.downloadLatest
@@ -30,23 +27,17 @@ module Pkg
     pkgdata = file['package']
     zipsupport = pkgdata['Zip']
     if zipsupport == true
-      # TODO: #13 add zipsupport to docs (package-example.ini)
       @RURL = pkgdata['Release']
-      puts 'Running zip download with curl'
       system("curl -O #{@RURL}")
     else
       @Isc = pkgdata['InstallCommand'] # install command
-      puts 'running Install command as specified in Pkgfile'
-      # puts "DEBUG-ISC: #{@Isc}"
-      # puts @Isc.to_s
       system(@Isc.to_s)
     end
   end
 end
-# FileUtils.touch('info.log')
 @param1 = ARGV[1]
 $os = Facter['osfamily'].value
-abort('ERROR:No task specified') while ARGV.empty?
+abort('That is not a command') while ARGV.empty?
 # Helper.man if ARGV[0] == 'man'
 Pkg.findPkg(@param1.to_s) if ARGV[0] == 'add'
 Pkg.findPkg(@param1.to_s) if ARGV[0] == 'install'
