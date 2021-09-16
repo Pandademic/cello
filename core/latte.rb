60abbe604c4f0f76e1a26dbc0e0be78c32455530
codefactor-io <support@codefactor.io>
2021-09-16 20:30:34 +0000

    [CodeFactor] Apply fixes to commit a891aa6

--- core/latte.rb
+++ core/latte.rb
@@ -12,9 +12,9 @@ module Pkg
   end
 
   def self.getPkgfile
-    #rescue OpenURI::HTTPError
-     # puts 'this package does not exist'
-    #end
+    # rescue OpenURI::HTTPError
+    # puts 'this package does not exist'
+    # end
     pfr = URI.open("https://raw.githubusercontent.com/Pandademic/Latte/master/pkgs/#{$query}.ini").read
     packageFileURL = "https://raw.githubusercontent.com/Pandademic/Latte/master/pkgs/#{$query}.ini"
     puts "Package file:#{pfr}"
