955cba74915204076386da5001fee0b59aba053b
Pandademic <stewie.gupte@gmail.com>
2021-10-31 08:23:29 -0400

    chomp

--- core/latte.rb
+++ core/latte.rb
@@ -38,8 +38,7 @@ module Pkg
   end
 end
 @param1=ARGV[1]
-$TRAY=ARGV[2]
-$TRAY.delete_suffix(':tray')
+$TRAY=ARGV[2].chomp(":tray")
 if ARGV[0] == 'add'
   puts "Starting install of #{@param1} from #{$TRAY}".colorize(:green)
   Pkg.findPkg @param1.to_s
