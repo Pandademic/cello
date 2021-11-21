fea175f69291a5f68693a6bba0a1d33f36aeb9c3
Pandademic <74566464+Pandademic@users.noreply.github.com>
2021-11-21 09:51:56 -0500

    Update engine.rb

--- core/engine.rb
+++ core/engine.rb
@@ -4,4 +4,60 @@ ______
 cello.rb is the most stable version of cello in the dev branch.All improvments will now be made on the engine file and then be coppied to cello.rb and on release to the stable branch
 =end
 #TODO: Add install instructions for engine & fix dir strucyure
-#FIXME:Only comment's no file structure
+# frozen_string_literal
+require 'inifile'
+require 'open-uri'
+require 'colorize'
+require 'faraday'
+# module to download Packages
+module Pkg
+  def self.findPkg(query)
+    if $query == ''
+      puts 'Empty package names are not allowed'.colorize(:red)
+      exit 1
+    end
+    Pkg.getPkgfile
+  end
+
+  def self.getPkgfile
+    if $TRAY == 'main'
+        packageFileURL="https://raw.githubusercontent.com/Pandademic/cello/master/pkgs/#{$query}.ini"
+    else
+        packageFileURL="https://raw.githubusercontent.com/#{$TRAY}/master/pkgs/#{$query}.ini"
+    end
+    res = Faraday.get(packageFileURL.to_s).status
+    if res.to_s == '404'
+      puts "#{$query} does not exist in #{$TRAY}".colorize(:red)
+      exit 1
+    end
+    system("curl  -O #{packageFileURL}")
+    puts 'package file download complete!'.colorize(:green)
+    Pkg.downloadLatest
+  end
+
+  def self.downloadLatest
+    file = IniFile.load("#{$query}.ini")
+    puts 'Package file read!'.colorize(:green)
+    pkgdata = file['package']
+    zipsupport = pkgdata['Media']
+    if zipsupport == true
+      @RURL = pkgdata['MediaUrl']
+      system("curl -O #{@RURL}")
+      puts "Installed #{$query} from #{$TRAY}".colorize(:green)
+    else
+      @Isc = pkgdata['InstallCommand'] # install command
+      system @Isc.to_s
+      puts "#{$query} from #{$TRAY} installed successfully".colorize(:green)
+    end
+    exit 0
+  end
+end
+$query = ARGV[1]
+$TRAY = ARGV[2].delete('--')
+if ARGV[0] == 'add'
+  puts "Starting install of #{$query} from #{$TRAY}".colorize(:green)
+  Pkg.findPkg $query
+else
+  puts 'Unknown Command'.colorize(:red)
+  exit 1
+end
