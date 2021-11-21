5b070bbe5474918fcf320823dcdd1089fe60fb4a
Pandademic <74566464+Pandademic@users.noreply.github.com>
2021-11-21 15:15:56 -0500

    Update engine.rb

--- core/engine.rb
+++ core/engine.rb
@@ -9,12 +9,23 @@ require 'inifile'
 require 'open-uri'
 require 'colorize'
 require 'faraday'
+# module to create errors (Engine 0.1.0)
+module Error
+	def self.emptyPkg
+		puts 'Empty package names are not allowed'.colorize(:red)
+    exit 1
+	end
+	def self.server404
+		 #EXPERIMENT: No param. direct var insert
+		 puts "server #{$TRAY} responded 404 for query #{$query} ".colorize(:red)
+     exit 1
+	end
+end
 # module to download Packages
 module Pkg
   def self.findPkg(query)
     if $query == ''
-      puts 'Empty package names are not allowed'.colorize(:red)
-      exit 1
+				Error.emptyPkg
     end
     Pkg.getPkgfile
   end
@@ -27,8 +38,7 @@ module Pkg
     end
     res = Faraday.get(packageFileURL.to_s).status
     if res.to_s == '404'
-      puts "#{$query} does not exist in #{$TRAY}".colorize(:red)
-      exit 1
+				Error.server404
     end
     system("curl  -O #{packageFileURL}")
     puts 'package file download complete!'.colorize(:green)
