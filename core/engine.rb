c59c7340a8d2f1245019734f0ba44a0f73bab483
Pandademic <stewie.gupte@gmail.com>
2021-11-21 08:37:35 -0500

    FEAT:Add engine file TODO: * add  code to new file * fix dir. structure * add installation instructions to readme * change contribution file

---
+++ core/engine.rb
@@ -0,0 +1,7 @@
+=begin
+engine.rb vs cello.rb
+______
+cello.rb is the most stable version of cello in the dev branch.All improvments will now be made on the engine file and then be coppied to cello.rb and on release to the stable branch
+=end
+#TODO: Add install instructions for engine & fix dir strucyure
+#FIXME:Only comment's no file structure
