891b81049e0363398fcd9a9b39322c805f996d9e
Pandademic <74566464+Pandademic@users.noreply.github.com>
2021-12-04 11:21:30 -0500

    Update cello.rb

--- src/cello.rb
+++ src/cello.rb
@@ -6,7 +6,7 @@ require 'faraday'
 # module to download Packages
 module Pkg
   def self.parse_pkg_name(_query)
-		if $query.empty?
+		if $userData[:query].empty?
       puts 'Empty package names are not allowed'.colorize(:red)
       exit 1
     end
@@ -14,14 +14,14 @@ module Pkg
   end
 
   def self.getPkgfile
-    if $TRAY == 'main'
-    	package_file_url = "https://raw.githubusercontent.com/Pandademic/cello/master/pkgs/#{$query}.ini"
+    if $userData[:tray] == 'main'
+    	package_file_url = "https://raw.githubusercontent.com/Pandademic/cello/master/pkgs/#{$userData[:query]}.ini"
     else
-    	package_file_url = "https://raw.githubusercontent.com/#{$TRAY}/master/pkgs/#{$query}.ini"
+    	package_file_url = "https://raw.githubusercontent.com/#{$userData[:tray]}/master/tray/#{$userData[:query]}.ini"
     end
     res = Faraday.get(package_file_url.to_s).status
     if res.to_s == '404'
-      puts "#{$query} does not exist in #{$TRAY}".colorize(:red)
+      puts "#{$userData[:query]} does not exist in #{$userData[:tray]}".colorize(:red)
       exit 1
     end
     system("curl  -O #{package_file_url}")
@@ -30,7 +30,7 @@ module Pkg
   end
 
   def self.downloadLatest
-    file = IniFile.load("#{$query}.ini")
+    file = IniFile.load("#{$userData[:query]}.ini")
     puts 'Package file read!'.colorize(:green)
     pkgdata = file['package']
     zipsupport = pkgdata['Media']
@@ -38,7 +38,7 @@ module Pkg
     if zipsupport
       @RURL = pkgdata['MediaUrl']
       system("curl -O #{@RURL}")
-      puts "Installed #{$query} from #{$TRAY}".colorize(:green)
+      puts "Installed #{$userData[:query]} from #{$userData[:tray]}".colorize(:green)
     else
       @Isc = pkgdata['InstallCommand'] # install command
       system @Isc.to_s
@@ -48,10 +48,10 @@ module Pkg
   end
 end
 $userData={:cmd => ARGV[0] ,:query => ARGV[1], :tray => ARGV[2].delete('--') }
-#$query = ARGV[1]
+#$query = ARGV[1] == $userData[:query]
 #$TRAY = ARGV[2].delete('--')
 if $userData[:cmd] == 'add'
-  puts "Starting install of #{$query} from #{$TRAY}".colorize(:green)
+  puts "Starting install of #{$userData[:query]} from #{$userData[:tray]}".colorize(:green)
   Pkg.parse_pkg_name $query
 else
   puts 'Unknown Command'.colorize(:red)
